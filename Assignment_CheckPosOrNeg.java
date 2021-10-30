9f0289e8af9d89b83b1156b957218a4b6b9a41e8
Yuvaraj98981 <93432207+Yuvaraj98981@users.noreply.github.com>
2021-10-30 18:07:01 +0530

    Add files via upload

---
+++ Assignment_CheckPosOrNeg.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Assignment_CheckPosOrNeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 35;
+		if (number == 0) {
+			System.out.println("The Number is neither Positive nor Negative");
+		} else if (number > 0) {
+			System.out.println("The Number is Positive");
+		} else {
+			System.out.println("The Number is Negative");
+		}
+
+	}
+
+}
