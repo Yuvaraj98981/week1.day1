89f85e6077e529f5ce12942017bab7b16ddd8ed7
Yuvaraj98981 <93432207+Yuvaraj98981@users.noreply.github.com>
2021-10-30 17:58:31 +0530

    Add files via upload

---
+++ Assignment_ConvertPosToNeg.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Assignment_ConvertPosToNeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = -40;
+		int result = 0;
+		if (number < 0) {
+			result = number * -1;
+		}
+		System.out.println("The Given Number " + number + " is converted in to " + result);
+	}
+
+}
